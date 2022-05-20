11e08f0ea37ec68a4406ff2cff4e095764991b20
Joe McNally <joe.t.mcnally@hotmail.com>
2022-05-21 00:35:43 +0100

    Refactor Account class
    Refactor the deposit and withdraw functions using private functions
    for checking whether valid inputs are given by the user. The first
    private function contains two further private functions for checking
    errors specifically regarding amount and date. The last private
    function checks specifically for balance and is outside the first
    private function because this only applies to the withdraw function.

--- src/account.js
+++ src/account.js
@@ -15,39 +15,47 @@ class Account {
   }
 
   deposit(amount, date) {
-    if (typeof amount != "number") {
-      throw "Invalid input given";
-    } else if (amount.toFixed(2) != amount) {
-      throw "Too many decimals! Smallest division is £0.01";
-    } else if (!this.#dateCheck(date)) {
-      throw "Invalid date";
-    }
+    this.#checkErrors(amount, date);
     this.balance += amount;
     this.log.addTransaction("deposit", amount, date, this.balance);
   }
 
   withdraw(amount, date) {
-    if (typeof amount != "number") {
-      throw "Invalid input given";
-    } else if (amount.toFixed(2) != amount) {
-      throw "Too many decimals! Smallest division is £0.01";
-    } else if (!this.#dateCheck(date)) {
-      throw "Invalid date";
-    } else if (amount > this.balance) {
-      throw "Insufficient balance";
-    }
+    this.#checkErrors(amount, date);
+    this.#balanceCheck(amount);
     this.balance -= amount;
     this.log.addTransaction("withdrawal", amount, date, this.balance);
   }
 
   // private methods
 
+  #checkErrors(amount, date) {
+    this.#amountCheck(amount);
+    this.#dateCheck(date);
+  }
+
+  #amountCheck(amount) {
+    if (typeof amount != "number") {
+      throw "Invalid input given";
+    } else if (amount.toFixed(2) != amount) {
+      throw "Too many decimals! Smallest division is £0.01";
+    }
+  }
+
+  #balanceCheck(amount) {
+    if (amount > this.balance) {
+      throw "Insufficient balance";
+    }
+  }
+
   #dateCheck(date) {
     const year = date.substring(6, 10);
     const month = date.substring(3, 5);
     const day = date.substring(0, 2);
     const formatDate = `${year}/${month}/${day}`;
-    return new Date(formatDate).getTime();
+    if (!new Date(formatDate).getTime()) {
+      throw "Invalid date";
+    }
   }
 }
 
