5e2d77176a417212bb73c2ccfca783a1ac12c796
Joe McNally <joe.t.mcnally@hotmail.com>
2022-05-20 20:25:46 +0100

    Add transactions to the log
    Create the addTransaction method in order to take the details of a
    transaction and incorporate them into an object with appropriate keys.
    This is so the information is easily retrieved by referencing a key.
    
    Then push this object into the log array to store the transaction.

--- src/transactionLog.js
+++ src/transactionLog.js
@@ -6,6 +6,16 @@ class TransactionLog {
   getLog() {
     return this.log;
   }
+
+  addTransaction(type, amount, date, balance) {
+    const transaction = {
+      type: type,
+      amount: amount,
+      date: date,
+      balance: balance,
+    };
+    this.log.push(transaction);
+  }
 }
 
 module.exports = TransactionLog;
--- src/transactionLog.test.js
+++ src/transactionLog.test.js
@@ -1,8 +1,25 @@
 const TransactionLog = require("./transactionLog");
 
 describe(TransactionLog, () => {
+  beforeEach(() => {
+    log = new TransactionLog();
+  });
+
   it("starts with an empty array of transactions", () => {
-    const log = new TransactionLog();
     expect(log.getLog()).toEqual([]);
+  });
+
+  describe("#addTransaction", () => {
+    it("adds an object representing a transaction into the log", () => {
+      log.addTransaction("deposit", 2000, "25/06/2022", 3500);
+      expect(log.getLog()).toEqual([
+        {
+          type: "deposit",
+          amount: 2000,
+          date: "25/06/2022",
+          balance: 3500,
+        },
+      ]);
+    });
   });
 });
