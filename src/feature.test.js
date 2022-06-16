023a6c53c69e52c62705bbebaffa50fc009faebc
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-16 11:43:17 +0100

    Amend feature test
    Alter the feature test to use the default value of date for each
    transation so that jest branch coverage reaches 100%. Test this
    by creating a new date in the feature test and expecting that date
    in short format appears in the statement for each transaction.

--- src/feature.test.js
+++ src/feature.test.js
@@ -3,21 +3,19 @@ const Account = require("./account");
 
 describe(Account, () => {
 	it("can make multiple transactions", () => {
-		const mockDateOne = new Date(2022, 4, 26);
-		const mockDateTwo = new Date(2022, 4, 27);
-		const mockDateThree = new Date(2022, 4, 29);
+		const today = new Date().toLocaleDateString();
 		const account = new Account();
-		account.deposit(1250, mockDateOne);
-		account.withdraw(450, mockDateTwo);
-		account.deposit(1000, mockDateThree);
+		account.deposit(1250);
+		account.withdraw(450);
+		account.deposit(1000);
 		expect(account.displayBalance()).toBe("Balance: £1800.00");
 		const logSpy = jest.spyOn(console, "log");
 		account.printStatement();
 		expect(logSpy).toHaveBeenCalledWith(
 			"date || credit || debit || balance"
-			+ "\n29/05/2022 || 1000.00 || || 1800.00"
-			+ "\n27/05/2022 || || 450.00 || 800.00"
-			+ "\n26/05/2022 || 1250.00 || || 1250.00"
+			+ `\n${today} || 1000.00 || || 1800.00`
+			+ `\n${today} || || 450.00 || 800.00`
+			+ `\n${today} || 1250.00 || || 1250.00`
 		);
 	});
 });
