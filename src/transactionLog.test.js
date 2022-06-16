542913f2976c5b37822111cab199caf6cfe34273
Joe McNally <joe.t.mcnally@hotmail.com>
2022-06-16 11:22:03 +0100

    Add Two more log tests
    In the transaction log tests, add one that tests a deposit being added
    when the history already contains at least one transaction and one that
    tests a withdrawal being added.
    
    Make these tests so that each outcome of #newBalanceCalculator is covered
    in unit testing when adding a transaction.

--- src/transactionLog.test.js
+++ src/transactionLog.test.js
@@ -4,6 +4,8 @@ const TransactionLog = require("./transactionLog");
 describe(TransactionLog, () => {
 	beforeEach(() => {
 		log = new TransactionLog();
+		mockDateOne = new Date(2022, 5, 25);
+		mockDateTwo = new Date(2022, 5, 26);
 	});
 
 	it("starts with an empty array of transactions", () => {
@@ -11,16 +13,28 @@ describe(TransactionLog, () => {
 	});
 
 	describe("#addTransaction", () => {
-		it("adds an object representing a transaction into the log", () => {
-			const mockDate = new Date(2022, 5, 25);
-			log.addTransaction("deposit", 2000, mockDate);
+		it("adds an object representing a deposit into an empty log", () => {
+			log.addTransaction("deposit", 2000, mockDateOne);
 			expect(log.getHistory()).toEqual([
-				{
-					type: "deposit",
-					amount: 2000,
-					date: mockDate,
-					balance: 2000,
-				},
+				{ type: "deposit", amount: 2000, date: mockDateOne, balance: 2000 }
+			]);
+		});
+
+		it("adds an object representing a deposit into a log which already contains a transaction", () => {
+			log.addTransaction("deposit", 2000, mockDateOne);
+			log.addTransaction("deposit", 1500, mockDateTwo);
+			expect(log.getHistory()).toEqual([
+				{ type: "deposit", amount: 1500, date: mockDateTwo, balance: 3500 },
+				{ type: "deposit", amount: 2000, date: mockDateOne, balance: 2000 }
+			]);
+		});
+
+		it("adds an object representing a withdrawal into the log", () => {
+			log.addTransaction("deposit", 2000, mockDateOne);
+			log.addTransaction("withdrawal", 500, mockDateTwo);
+			expect(log.getHistory()).toEqual([
+				{ type: "withdrawal", amount: 500, date: mockDateTwo, balance: 1500 },
+				{ type: "deposit", amount: 2000, date: mockDateOne, balance: 2000 }
 			]);
 		});
 
